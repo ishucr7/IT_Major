479c45797f54f6adce27c2db43018737b0cfac9f
Apoorav29 <apoorav.singh@students.iiit.ac.in>
2017-04-03 19:14:44 +0530

    made some changes

--- app/courier/controllers.py
+++ app/courier/controllers.py
@@ -6,15 +6,13 @@ import time
 
 mod_courier=Blueprint('courier',__name__)
 
-@mode_courier('/today',methods=['GET'])
+@mode_courier('/today',methods=['GET','POST'])
 def show_todays():
     date=time.strftime("%d/%m/%Y")
     couriers=Courier.query.filter(Courier.date==date).all()
     return render_template("../templates/user/user.html",couriers)
 
-@mod_courier('/all',__name__)
+@mod_courier('/couriers',methods=['GET','POST'])
 def show_all():
     couriers=Courier.query.all()
     return render_template("../templates/user/all_courier.html",couriers)
-
-
