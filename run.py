6c4c07ee0d13a495c57b01356cfa254fdb01c968
Aashishgaba097 <aashishgaba097@gmail.com>
2017-04-04 22:36:31 +0530

    hey

--- run.py
+++
@@ -1,9 +0,0 @@
-from flask import *
-from app import app, db
-from flask_cors import CORS
-CORS(app)
-
-if __name__ == '__main__':
-    db.create_all()
-    # app.run(host='0.0.0.0', port=8080, debug=True)
-    app.run(port=8080, debug=True)
