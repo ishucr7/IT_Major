28473f5567bf4dfd14cbee04e01b5a7f16fdd22f
Aashishgaba097 <aashishgaba097@gmail.com>
2017-04-08 03:05:40 +0530

    doit / change

--- boilerplate/app/__init__.py
+++ boilerplate/app/__init__.py
@@ -1,5 +1,5 @@
 # Import flask and template operators
-from flask import Flask, render_template
+from flask import Flask, render_template,session,jsonify
 
 # Import SQLAlchemy
 from flask_sqlalchemy import SQLAlchemy
@@ -10,8 +10,10 @@ app = Flask(__name__)
 # Configurations
 app.config.from_object('config')
 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 # Define the database object which is imported
 # by modules and controllers
+
 db = SQLAlchemy(app)
 
 # Sample HTTP error handling
@@ -21,12 +23,15 @@ def not_found(error):
     return render_template('404.html'), 404
 
 # Import a module / component using its blueprint handler variable (mod_auth)
+
 from app.user.controllers import mod_user
+
 #from app.albums.controllers import mod_albums
 #from app.photos.controllers import mod_photos
 
 # Register blueprint(s)
 app.register_blueprint(mod_user)
+
 #app.register_blueprint(mod_albums)
 #app.register_blueprint(mod_photos)
 # app.register_blueprint(xyz_module)
