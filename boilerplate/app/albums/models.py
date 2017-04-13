c938cbeca52ffb248099a9e68d839d9184f474c4
Apoorav29 <apoorav.singh@students.iiit.ac.in>
2017-04-13 23:30:00 +0530

    made app working with python3

--- boilerplate/app/albums/models.py
+++ boilerplate/app/albums/models.py
@@ -1,38 +1,55 @@
-from flask_sqlalchemy import SQLAlchemy
-from app import db,app
-from werkzeug.security import generate_password_hash, check_password_hash
-# from app.user.models import User
-#from app.photos.models import Photo
-
-class Album(db.Model):
-    __tablename__ = 'photos'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(255))
-    datetime = db.Column(db.DateTime)
-    likes = db.Column(db.Integer)
-    dislikes = db.Column(db.Integer)
-    privacy = db.Column(db.String(255))
-    photo_id=db.Column(db.Integer,db.ForeignKey('photo.id'))
-    # perrsonalb_id = db.Column(db.Integer,db.ForeignKey('user.id'))
-
-    def __init__(self, name, datetime,likes,dislikes,privacy,photos):
-        self.name = name
-        self.datetime=datetime
-        self.likes= likes
-        self.dislikes=dislikes
-        self.privacy=privacy
-        self.photos=photos
+    #!/usr/bin/python
+    # -*- coding: utf-8 -*-
+    from flask_sqlalchemy import SQLAlchemy
+    from app import db, app
+    from werkzeug.security import generate_password_hash, \
+        check_password_hash
 
 
-    def to_dict(self):
-        return {
-            'id' : self.id,
-            'name': self.name,
-            'datetime': self.datetime,
-        	'dislikes': self.dislikes,
-        	'likes': self.likes,
-        	'photos': self.photos
-        }
+    class Album(db.Model):
 
-    def __repr__(self):
-        return "User<%d> %s" % (self.id, self.name)
+        __tablename__ = 'photos'
+        id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+        name = db.Column(db.String(255))
+        datetime = db.Column(db.DateTime)
+        likes = db.Column(db.Integer)
+        dislikes = db.Column(db.Integer)
+        privacy = db.Column(db.String(255))
+        photo_id = db.Column(db.Integer, db.ForeignKey('photo.id'))
+
+        # perrsonalb_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+
+        def __init__(
+            self,
+            name,
+            datetime,
+            likes,
+            dislikes,
+            privacy,
+            photos,
+            ):
+            self.name = name
+            self.datetime = datetime
+            self.likes = likes
+            self.dislikes = dislikes
+            self.privacy = privacy
+            self.photos = photos
+
+        def to_dict(self):
+            return {
+                'id': self.id,
+                'name': self.name,
+                'datetime': self.datetime,
+                'dislikes': self.dislikes,
+                'likes': self.likes,
+                'photos': self.photos,
+                }
+
+        def __repr__(self):
+            return 'User<%d> %s' % (self.id, self.name)
+
+
+
+
+
+			
--- boilerplate/app/photos/models.py
+++ boilerplate/app/photos/models.py
@@ -1,47 +1,70 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from flask import *
 from flask_sqlalchemy import SQLAlchemy
-from app import db,app
-from werkzeug.security import generate_password_hash, check_password_hash
+from app import db, app
+from werkzeug.security import generate_password_hash, \
+    check_password_hash
+
+
 # from app.user.models import User
-#from app.albums.models import Album
+# from app.albums.models import Album
 
 class Photo(db.Model):
-    __tablename__='photo'
-    id=db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name=db.Column(db.String(255))
-    #photo_url=db.Column(postgresql.ARRAY(String(255),primary_key=True)
-    datetime=db.Column(db.DateTime)
-    likes=db.Column(db.Integer)
-    dislikes=db.Column(db.Integer)
-    privacy=db.Column(db.String(255))
-    # albums=db.relationship('Albums',backref='photo',lazy='dynamic')
-    person_id=db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, name, datetime,privacy):
+    __tablename__ = 'photo'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255))
+
+    # photo_url=db.Column(postgresql.ARRAY(String(255),primary_key=True)
+
+    datetime = db.Column(db.DateTime)
+    likes = db.Column(db.Integer)
+    dislikes = db.Column(db.Integer)
+    privacy = db.Column(db.String(255))
+
+    # albums=db.relationship('Albums',backref='photo',lazy='dynamic')
+
+    person_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __init__(
+        self,
+        name,
+        datetime,
+        privacy,
+        ):
+
         self.name = name
-        self.datetime=datetime
-        self.likes= 0
-        self.dislikes=0
-        self.privacy=privacy
+        self.datetime = datetime
+        self.likes = 0
+        self.dislikes = 0
+        self.privacy = privacy
 
     #  def check_password(self, password):
     #     return check_password_hash(self.password, password)
-    def likefunc():
-        self.likes+=1
-    def dislikefunc():
-        self.dislikes+=1
-    def assignurl(url):
-	self.photo_url.append(url);
 
+    def likefunc():
+        self.likes += 1
+
+    def dislikefunc():
+        self.dislikes += 1
+
+    def assignurl(url):
+        self.photo_url.append(url)
 
     def to_dict(self):
         return {
-            'id' : self.id,
+            'id': self.id,
             'name': self.name,
-        	'datetime': self.datetime,
-        	'dislikes': self.dislikes,
-        	'likes': self.likes,
-        }
+            'datetime': self.datetime,
+            'dislikes': self.dislikes,
+            'likes': self.likes,
+            }
 
     def __repr__(self):
-        return "User<%d> %s" % (self.id, self.name)
+        return 'User<%d> %s' % (self.id, self.name)
+
+
+
+			
